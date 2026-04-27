ffa8799e40531fa829ccaf1760ac519bda722691
lucasamarale <187224735+lucasamarale@users.noreply.github.com>
2026-04-27 20:35:19 -0300

    Corrige conexao.php: senha vazia e porta padrao 3306 para compatibilidade

--- conexao.php
+++ conexao.php
@@ -2,12 +2,12 @@
 $host     = "127.0.0.1";
 $user     = "root";
 $porta    = "3306";
-$password = "ceub123456";
+$password = "";
 $db       = "tarefas";
 
 $conexao = new PDO(
-    'mysql:host=' . $host . ';port=' . $porta . ';dbname=' . $db,
-    $user,
-    $password
-);
+        'mysql:host=' . $host . ';port=' . $porta . ';dbname=' . $db,
+        $user,
+        $password
+    );
 ?>
